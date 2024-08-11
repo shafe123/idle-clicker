c2731e2245ace54a8698182c31a3c7dc60677d2a
Ethan Shafer <ethan.h.shafer@gmail.com>
2024-08-11 05:55:49 -0400

    added service test cases

--- src/services/data-loader.service.spec.ts
+++ src/services/data-loader.service.spec.ts
@@ -5,7 +5,7 @@ import { DataLoaderService } from './data-loader.service';
 describe('DataLoaderService', () => {
   let service: DataLoaderService;
 
-  beforeEach(() => {
+  beforeAll(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(DataLoaderService);
   });
@@ -13,4 +13,20 @@ describe('DataLoaderService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have armor', () => {
+    expect(service.armors).toBeTruthy();
+    expect(service.armors.length).toBeGreaterThan(0);
+  });
+
+  it('should have weapons', () => {
+    expect(service.weapons).toBeTruthy();
+    expect(service.weapons.length).toBeGreaterThan(0);
+  });
+
+  it('should have enemies', () => {
+    expect(service.enemies).toBeTruthy();
+    expect(service.enemies.length).toBeGreaterThan(0);
+  });
+
 });
